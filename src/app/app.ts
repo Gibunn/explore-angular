301af6636954a2bff1f139712eb584034b81542d
MOVRA <abgibun@gmail.com>
2026-07-16 10:29:09 +0700

    composition with components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Button } from './components/button/button';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+	selector: 'app-root',
+	imports: [RouterOutlet, Button],
+	templateUrl: './app.html',
+	styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('explore-angular');
+	count = signal(0)
+
+	increment() {
+		this.count.update((prev) => prev + 1)
+	}
+
+	decrement() {
+		this.count.update((prev) => prev + 1)
+	}
 }
